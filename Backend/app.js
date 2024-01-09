6ad7c4e7ce2a7e5af366d9c818411a8bde59074c
vk-develops <vimalkumarv291@gmail.com>
2024-01-09 13:18:55 +0530

    Initialized the app and added necessary folders

---
+++ Backend/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+//App init
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Built-in Middlewares and Imported ones
+app.use(express.json());
+app.use(cors());
+
+//HTTP GET Method Test
+app.get("/", (req, res) => {
+    res.status(200).json({ success: true, message: "HTTP Method Success!" });
+});
+
+//API's
+
+//Custom Middlewares
+
+//App listen
+app.listen(PORT, () => {
+    console.log(`Server started and running on http://localhost:${PORT}`);
+});
