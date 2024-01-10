0805c95eed7e8d460eeadab94ee15383c067036c
vk-develops <vimalkumarv291@gmail.com>
2024-01-10 16:37:35 +0530

    feat: Added the login page

--- Frontend/src/Components/Footer.jsx
+++ Frontend/src/Components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="bg-slate-900 w-full py-5 mx-auto border-t-2 border-slate-800">
+            <p className="text-base font-regular text-white text-center">
+                Copyright &copy; 2024 @MovieMatic
+            </p>
+        </footer>
+    );
 };
 
 export default Footer;
--- Frontend/src/Router/Router.jsx
+++ Frontend/src/Router/Router.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import LoginPage from "../Pages/LoginPage";
+import HomePage from "../Pages/HomePage";
+import RegisterPage from "../Pages/RegisterPage";
+import Layout from "../Components/Layout";
 
 const Router = () => {
     return (
         <Routes>
-            <Route />
+            <Route
+                path="/"
+                element={<Layout />}
+            >
+                <Route
+                    index
+                    element={<HomePage />}
+                />
+                <Route
+                    path="login"
+                    element={<LoginPage />}
+                />
+                <Route
+                    path="register"
+                    element={<RegisterPage />}
+                />
+            </Route>
         </Routes>
     );
 };
