041a1d245a78c71270fd555b3bfc4322d0d9237e
vk-develops <vimalkumarv291@gmail.com>
2024-01-10 08:38:18 +0530

    Created frontend project and added necessary packages

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="grid place-content-center">
+            <h1 className="underline">App</h1>
+        </div>
+    );
+};
+
+export default App;
