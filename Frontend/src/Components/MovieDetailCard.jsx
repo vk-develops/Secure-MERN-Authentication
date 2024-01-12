b7181033a76b6929ec45bcc905cb151d2aacdb53
vk-develops <vimalkumarv291@gmail.com>
2024-01-12 23:15:26 +0530

    feat: Added the full functionality of the movie detail page

--- Frontend/src/Components/MovieDetailCard.jsx
+++ Frontend/src/Components/MovieDetailCard.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-const MovieDetailCard = () => {
+const MovieDetailCard = ({ movieDetail }) => {
+    const imdbValue =
+        movieDetail.Ratings && movieDetail.Ratings.length > 0
+            ? movieDetail.Ratings[0].Value
+            : "N/A";
+
     return (
         <div className="h-screen max-w-4xl mx-auto py-5">
-            <div className="w-full bg-slate-800 p-6 rounded-2xl flex items-start justify-start">
+            <div className="w-full bg-slate-800 p-6 rounded-2xl flex items-start gap-5 justify-start">
                 <div className=" w-2/4">
                     <img
-                        className="rounded-lg h-96"
-                        src="https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg"
+                        className="rounded-lg h-96 w-full object-cover"
+                        src={movieDetail.Poster}
                         alt="Movie Poster"
                     />
                 </div>
@@ -15,60 +20,57 @@ const MovieDetailCard = () => {
                     <div className="flex items-center justify-between">
                         <div className="flex items-baseline justify-start gap-2">
                             <h1 className="text-white font-bold text-4xl">
-                                Thor love and thunder
+                                {movieDetail.Title}
                             </h1>
                             <h6 className="text-yellow-400 font-medium text-sm">
-                                (7.0/10)
+                                ({imdbValue})
                             </h6>
                         </div>
-                        <h6 className="py-1 px-2 bg-slate-500 text-white font-medium text-xs rounded-md">
-                            Movie
+                        <h6 className="py-1 px-2 capitalize bg-slate-500 text-white font-medium text-xs rounded-md">
+                            {movieDetail.Type}
                         </h6>
                     </div>
 
                     <p className="text-slate-400 text-base font-normal pt-5 pb-1">
-                        The powerful but arrogant god Thor is cast out of Asgard
-                        to live amongst humans in Midgard (Earth), where he soon
-                        becomes one of their finest defenders.
+                        {movieDetail.Plot}
                     </p>
 
                     <div className="flex items-center justify-between py-4">
                         <h5 className="text-slate-300 font-normal text-lg">
-                            Genre: Action, Fantasy
+                            Genre: {movieDetail.Genre}
                         </h5>
                         <h5 className="text-slate-300 font-normal text-lg">
-                            Year: 2024
+                            Year: {movieDetail.Year}
                         </h5>
                         <h5 className="text-slate-300 font-normal text-lg">
-                            Rated: PG-13
+                            Rated: {movieDetail.Rated}
                         </h5>
                     </div>
 
                     <div className="flex items-center justify-between pb-4">
                         <h5 className="text-slate-300 font-normal text-lg">
-                            Director: Kenneth Branagh
+                            Director: {movieDetail.Director}
                         </h5>
                         <h5 className="text-slate-300 font-normal text-lg">
-                            Writers: Ashley Miller, Zack Stentz, Don Payne
+                            Writers: {movieDetail.Writer}
                         </h5>
                     </div>
 
                     <div className="flex items-center justify-between py-4">
                         <h5 className="text-slate-300 font-normal text-lg">
-                            Released: 06 May 2011
+                            Released: {movieDetail.Released}
                         </h5>
                         <h5 className="text-slate-300 font-normal text-lg">
-                            Runtime: 115 min
+                            Runtime: {movieDetail.Runtime}
                         </h5>
                         <h5 className="text-slate-300 font-normal text-lg">
-                            Box-Office: $181,030,624
+                            Box-Office: {movieDetail.BoxOffice}
                         </h5>
                     </div>
 
                     <div>
                         <h5 className="text-slate-300 font-normal text-lg pt-1">
-                            Actors: Chris Hemsworth, Anthony Hopkins, Natalie
-                            Portman
+                            Actors: {movieDetail.Actors}
                         </h5>
                     </div>
                 </div>
--- Frontend/src/Pages/MovieDetailPage.jsx
+++ Frontend/src/Pages/MovieDetailPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import MovieDetailCard from "../Components/MovieDetailCard";
 
 const MovieDetailPage = () => {
@@ -27,7 +27,19 @@ const MovieDetailPage = () => {
 
     return (
         <div>
-            <MovieDetailCard />
+            <div className="max-w-4xl mx-auto py-5 mt-5">
+                <Link
+                    to="/"
+                    className="bg-slate-700 border-2 border-slate-500 text-slate-300 font-medium text-base py-3 px-12"
+                >
+                    Back to home
+                </Link>
+            </div>
+            {movie ? (
+                <MovieDetailCard movieDetail={movie} />
+            ) : (
+                <h1>Movie detail not found!!!</h1>
+            )}
         </div>
     );
 };
