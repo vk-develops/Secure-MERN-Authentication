a095fa256358bb8f9829075e6cfeaffafc248184
vk-develops <vimalkumarv291@gmail.com>
2024-01-12 22:14:37 +0530

    feat: Created the movie detail functionality

--- Frontend/src/Pages/MovieDetailPage.jsx
+++ Frontend/src/Pages/MovieDetailPage.jsx
@@ -1,7 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const MovieDetailPage = () => {
+    const { title } = useParams();
+
+    const [movie, setMovie] = useState([]);
+
+    const movieByParams = async () => {
+        try {
+            const response = await fetch(
+                `${import.meta.env.VITE_MOVIE_APP_URI}&t=${title}`
+            );
+
+            const data = await response.json();
+            console.log(data);
+            setMovie(data);
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    useEffect(() => {
+        movieByParams();
+    }, [title]);
+
     return <div>MovieDetailPage</div>;
 };
 
