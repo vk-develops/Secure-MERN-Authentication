3377b7166ffe2272d61f5ec908d192a3f1256d2a
vk-develops <vimalkumarv291@gmail.com>
2024-01-30 20:45:33 +0530

    feat: Added the UI for the admin page modal

--- Frontend/src/Pages/Admin/AdminPage.jsx
+++ Frontend/src/Pages/Admin/AdminPage.jsx
@@ -1,4 +1,47 @@
 import React, { useEffect, useState } from "react";
+import { IoClose } from "react-icons/io5";
+
+const ModalComponent = () => {
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal);
+    };
+
+    return (
+        <section
+            style={{
+                backgroundColor: "rgb(148, 163, 184, .8)",
+                backdropFilter: blur("200px"),
+            }}
+            className="w-full h-screen fixed top-0 left-0 z-0 overflow-y-hidden"
+        >
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-36 bg-white p-12">
+                <div className="relative">
+                    <button className="absolute -top-5 right-0">
+                        <IoClose size={24} />
+                    </button>
+                </div>
+                <h1 className="font-bold text-2xl text-slate-800">
+                    Confirm Deletion
+                </h1>
+                <p className="font-medium text-base text-slate-600 pt-3">
+                    Are you sure, do you want to delete the user @Username and
+                    ID: @123456789123456789. Once you deleted you cannot revert
+                    your action, make sure before deleting a user
+                </p>
+                <div className="flex items-center justify-end mt-10 gap-4">
+                    <button className="px-8 py-2 bg-slate-500 rounded-md text-base font-medium text-white">
+                        Close
+                    </button>
+                    <button className="px-8 py-2 bg-teal-500 rounded-md text-base font-medium text-white">
+                        Yes, Delete
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+};
 
 const AdminPage = () => {
     const [users, setUsers] = useState([]);
@@ -34,53 +77,64 @@ const AdminPage = () => {
     }, []);
 
     return (
-        <section className="max-w-xl h-screen mx-auto">
-            <div className="mt-10">
-                <h1 className="font-bold text-4xl text-white">Admin Pannel</h1>
-                <p className="font-regular text-base text-slate-400 py-5">
-                    Welcome, Admin! Dive into the heart of Movie Matic with our
-                    comprehensive admin dashboard. Here, you have full
-                    visibility into user details, enabling you to manage and
-                    monitor user accounts effortlessly.
-                </p>
-            </div>
-            <div>
-                <h2 className="font-bold text-xl text-white mb-3">Users</h2>
-                <table className="w-full border-collapse border border-slate-300">
-                    <thead>
-                        <tr className="bg-slate-300">
-                            <th className="border border-gray-600 py-2 px-4">
-                                ID
-                            </th>
-                            <th className="border border-gray-600 py-2 px-4">
-                                Name
-                            </th>
-                            <th className="border border-gray-600 py-2 px-4">
-                                Email
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((user) => (
-                            <tr
-                                key={user._id}
-                                className="hover:bg-slate-500 bg-slate-400"
-                            >
-                                <td className="border border-gray-600 py-2 px-4">
-                                    {user._id}
-                                </td>
-                                <td className="border border-gray-600 py-2 px-4">
-                                    {user.name}
-                                </td>
-                                <td className="border border-gray-600 py-2 px-4">
-                                    {user.email}
-                                </td>
+        <>
+            <ModalComponent />
+            <section className="max-w-3xl h-screen mx-auto">
+                <div className="mt-10">
+                    <h1 className="font-bold text-4xl text-white">
+                        Admin Pannel
+                    </h1>
+                    <p className="font-regular text-base text-slate-400 py-5">
+                        Welcome, Admin! Dive into the heart of Movie Matic with
+                        our comprehensive admin dashboard. Here, you have full
+                        visibility into user details, enabling you to manage and
+                        monitor user accounts effortlessly.
+                    </p>
+                </div>
+                <div>
+                    <h2 className="font-bold text-xl text-white mb-3">Users</h2>
+                    <table className="w-full border-collapse border border-slate-300">
+                        <thead>
+                            <tr className="bg-slate-300">
+                                <th className="border border-gray-600 py-2 px-4">
+                                    ID
+                                </th>
+                                <th className="border border-gray-600 py-2 px-4">
+                                    Name
+                                </th>
+                                <th className="border border-gray-600 py-2 px-4">
+                                    Email
+                                </th>
+                                <th className="border border-gray-600 py-2 px-4">
+                                    Options
+                                </th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </section>
+                        </thead>
+                        <tbody>
+                            {users.map((user) => (
+                                <tr
+                                    key={user._id}
+                                    className="hover:bg-slate-500 bg-slate-400"
+                                >
+                                    <td className="border border-gray-600 py-2 px-4">
+                                        {user._id}
+                                    </td>
+                                    <td className="border border-gray-600 py-2 px-4">
+                                        {user.name}
+                                    </td>
+                                    <td className="border border-gray-600 py-2 px-4">
+                                        {user.email}
+                                    </td>
+                                    <td className="border border-gray-600 py-2 px-4">
+                                        <button>Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+        </>
     );
 };
 
