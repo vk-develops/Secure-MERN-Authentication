f73587521e379486f12bcbd53fe8f62ecbaa5df0
vk-develops <vimalkumarv291@gmail.com>
2024-03-02 21:53:25 +0530

    feat: Added the profil and edit profile page

--- Frontend/src/Pages/ProfilePage.jsx
+++ Frontend/src/Pages/ProfilePage.jsx
@@ -1,8 +1,16 @@
 import React, { useContext } from "react";
 import { UserContext } from "../Context/UserContext";
+import { Link } from "react-router-dom";
+import useLogout from "../Hooks/useLogout";
 
 const ProfilePage = () => {
     const { user } = useContext(UserContext);
+
+    const { logout } = useLogout();
+
+    const handleLogout = async () => {
+        await logout();
+    };
 
     return (
         user && (
@@ -22,26 +30,40 @@ const ProfilePage = () => {
                                 Email: {user.email}
                             </h5>
                             <h5 className="text-slate-300 text-lg font-medium mb-5">
+                                {user && user.about
+                                    ? `About: ${user.about}`
+                                    : null}
+                            </h5>
+                            <h5 className="text-slate-300 text-lg font-medium mb-5">
                                 Verified: Yes,
                             </h5>
                             <h5 className="text-slate-300 text-lg font-medium mb-5">
-                                Ph no: 1234567890
+                                Ph no:{" "}
+                                {user && user.phno
+                                    ? `${user.phno}`
+                                    : "9876543210"}
                             </h5>
                             <h5 className="text-slate-300 text-lg font-medium mb-5">
-                                Address: street number (123), street name (Main
+                                Address:{" "}
+                                {user && user.address
+                                    ? user.address
+                                    : `Address: street number (123), street name (Main
                                 Street), city (Anytown), state (USA), and zip
-                                code (12345).
+                                code (12345).`}
                             </h5>
                         </div>
 
                         <div className="flex items-center justify-start gap-5 my-10 flex-wrap">
-                            <button className="text-lg font-medium px-10 py-2 bg-slate-500 text-white rounded-full">
+                            <Link
+                                to={`edit-profile`}
+                                className="text-lg font-medium px-10 py-2 bg-slate-500 text-white rounded-full"
+                            >
                                 Edit Profile
-                            </button>
-                            <button className="text-lg font-medium px-10 py-2 bg-slate-500 text-white rounded-full">
-                                Reset Password
-                            </button>
-                            <button className="text-lg font-medium px-10 py-2 bg-slate-500 text-white rounded-full">
+                            </Link>
+                            <button
+                                onClick={handleLogout}
+                                className="text-lg font-medium px-10 py-2 bg-slate-500 text-white rounded-full"
+                            >
                                 Logout
                             </button>
                         </div>
@@ -49,7 +71,11 @@ const ProfilePage = () => {
                     <div className="w-2/4">
                         <img
                             className="h-96 w-96 rounded-full object-cover"
-                            src="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?cs=srgb&dl=pexels-suliman-sallehi-1704488.jpg&fm=jpg"
+                            src={
+                                user.image
+                                    ? user.image
+                                    : "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?cs=srgb&dl=pexels-suliman-sallehi-1704488.jpg&fm=jpg"
+                            }
                             alt="Profile image"
                         />
                     </div>
