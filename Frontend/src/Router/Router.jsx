cbc6ce2efbd33cd9e8e08f0fe8f2f3f9823e95e0
vk-develops <vimalkumarv291@gmail.com>
2024-03-02 21:44:08 +0530

    fix: Fixed the scrolling issue in the app when redirecting

--- Frontend/src/Router/Router.jsx
+++ Frontend/src/Router/Router.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import LoginPage from "../Pages/LoginPage";
 import HomePage from "../Pages/HomePage";
 import RegisterPage from "../Pages/RegisterPage";
@@ -15,6 +15,12 @@ import AdminPage from "../Pages/Admin/AdminPage";
 import EditProfilePage from "../Pages/EditProfilePage";
 
 const Router = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
     return (
         <Routes>
             <Route
