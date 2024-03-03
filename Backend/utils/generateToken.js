77d8a4193f11f66ec8ddd014d712d2e29e36bf3b
vk-develops <vimalkumarv291@gmail.com>
2024-03-03 18:04:01 +0530

    changes

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,17 +1,20 @@
-import jwt from "jsonwebtoken";
-
 const generateToken = (res, userId) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
-        expiresIn: "1d",
-    });
+    try {
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "1d",
+        });
 
-    //Set cookie
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
-        maxAge: 1 * 24 * 60 * 60 * 1000,
-    });
+        // Set cookie
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "None", // Adjust as needed based on your scenario
+            maxAge: 1 * 24 * 60 * 60 * 1000,
+        });
+    } catch (error) {
+        console.error("Error generating token:", error);
+        // Handle the error, e.g., return an error response
+    }
 };
 
 export default generateToken;
