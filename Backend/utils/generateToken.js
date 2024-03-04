0ca300a1cd8fef70354db831adc09e49db3a5d3b
vk-develops <vimalkumarv291@gmail.com>
2024-03-04 18:12:19 +0530

    modified the same site attribute

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -12,6 +12,7 @@ const generateToken = (res, userId) => {
         res.cookie("jwt", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV !== "development",
+            sameSite: "None",
             maxAge: 1 * 24 * 60 * 60 * 1000,
         });
 
