c1da7a9d273fcb3004f7d0caa709cd295925fcf1
vk-develops <vimalkumarv291@gmail.com>
2024-03-04 17:46:36 +0530

    modified the cookie settings

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -12,9 +12,7 @@ const generateToken = (res, userId) => {
         res.cookie("jwt", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV !== "development",
-            sameSite: "none",
             maxAge: 1 * 24 * 60 * 60 * 1000,
-            domain: ".vercel.app", //common root domain
         });
 
         console.log("Cookie Set Successfully");
