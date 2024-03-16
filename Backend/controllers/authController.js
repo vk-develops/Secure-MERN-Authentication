10c9dd2ebc91648a698fc14510c8802a28cf3943
vk-develops <vimalkumarv291@gmail.com>
2024-03-16 22:02:06 +0530

    fix: Fixed the logout api

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -172,15 +172,16 @@ const loginUser = asyncHandler(async (req, res) => {
 // @access  Private
 const logoutUser = asyncHandler(async (req, res) => {
     try {
+        //Removing the cookie
         res.cookie("jwt", "", {
             httpOnly: true,
-            secure: process.env.NODE_ENV !== "development",
-            sameSite: "None",
             expires: new Date(0),
         });
+
+        //Response
         res.status(200).json({
             success: true,
-            message: "User logout successful",
+            message: "Account logout success.",
         });
     } catch (err) {
         console.log(err.message);
