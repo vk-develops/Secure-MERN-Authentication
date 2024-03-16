0f2736ccde67371c62a2f3c0fe19b0379f1b505c
vk-develops <vimalkumarv291@gmail.com>
2024-03-16 22:43:36 +0530

    build: Production ready build

--- Backend/index.js
+++ Backend/index.js
@@ -2,12 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
 import userRoute from "./routes/userRoute.js";
 import accountRoute from "./routes/accountRoute.js";
 import adminRoute from "./routes/adminRoute.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+
+//Constants
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //App init
 dotenv.config();
@@ -26,6 +31,9 @@ app.use(
 );
 app.use(cookieParser());
 
+//Express static
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
 //HTTP GET Method Test
 app.get("/", (req, res) => {
     res.status(200).json({ success: true, message: "HTTP Method Success!" });
@@ -38,6 +46,11 @@ app.use("/api/v1/users/account", accountRoute);
 
 //Admin Api
 app.use("/api/v1/admin", adminRoute);
+
+//Express Static
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+});
 
 //Custom Middlewares
 app.use(notFound);
--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -11,7 +11,6 @@ const generateToken = (res, userId) => {
             httpOnly: true,
             secure: process.env.NODE_ENV !== "development",
             sameSite: "strict",
-            // maxAge: 2 * 60 * 1000,
             maxAge: 1 * 24 * 60 * 60 * 1000,
         });
     } catch (error) {
